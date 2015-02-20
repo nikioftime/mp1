1b7c3a706070e68c3c9a4374ce6feab367234317
nikioftime <titanswimr@gmail.com>
2015-02-20 05:39:22 +0000

    see if can host

--- js/script.js
+++ js/script.js
@@ -1,16 +1,73 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
-
-function highlightThis(event) {
-    //event.stopPropagation();
+// var divs = document.getElementsByTagName('div');
+// for(var i=0; i<divs.length; i++) {
+//   divs[i].addEventListener("click", highlightThis);
   
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
+//   divs[i].addEventListener("click", highlightThis, true);
+//   divs[i].addEventListener("click", highlightThis, false);
+// }
+
+// function highlightThis(event) {
+//     //event.stopPropagation();
+  
+//     var backgroundColor = this.style.backgroundColor;
+//     this.style.backgroundColor='yellow';
+//     alert(this.className);
+//     this.style.backgroundColor=backgroundColor;
+// }
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top - 50
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
+$(window).scroll(function() {
+	$("#navhome").removeClass("position");
+	var offset = window.pageYOffset || document.documentElement.scrollTop;
+	if (offset >= 10)
+	{
+		$("header").addClass("smaller");
+	}
+	else {
+		$('header').removeClass("smaller");
+	}
+
+	var homeoff = $("section#home").offset().top;
+	var abtoff = $("section#about").offset().top;
+	var projoff = $("section#projects").offset().top;
+	var whatoff = $("section#whatever").offset().top;
+	var stufoff = $("section#stuff").offset().top;
+
+	if (offset < abtoff - 50) {
+		$("#navhome").addClass("position");
+		$("#navabt").removeClass("position");
+	}
+	else if (offset < projoff - 50) {
+		$("#navhome").removeClass("position");
+		$("#navabt").addClass("position");
+		$("#navproj").removeClass("position");
+	}
+	else if (offset < whatoff - 50) {
+		$("#navabt").removeClass("position");
+		$("#navproj").addClass("position");
+		$("#navwhat").removeClass("position");
+	}
+	else if (offset < stufoff - 50) {
+		$("#navproj").removeClass("position");
+		$("#navwhat").addClass("position");
+		$("#navstuff").removeClass("position");
+	}
+	else {
+		$("#navwhat").removeClass("position");
+		$("#navstuff").addClass("position");
+	};
+});
