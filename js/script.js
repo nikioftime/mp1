30faaaaf1c685843341c272b526d45e46a405a04
nikioftime <titanswimr@gmail.com>
2015-02-21 04:20:44 +0000

    final commit

--- js/script.js
+++ js/script.js
@@ -28,6 +28,9 @@ $(document).ready(function(){
 	        window.location.hash = target;
 	    });
 	});
+	$("#navabt").removeClass("position");
+	$("#navproj").removeClass("position");
+	$("#navdog").removeClass("position");
 });
 
 $(window).scroll(function() {
@@ -44,8 +47,7 @@ $(window).scroll(function() {
 	var homeoff = $("section#home").offset().top;
 	var abtoff = $("section#about").offset().top;
 	var projoff = $("section#projects").offset().top;
-	var whatoff = $("section#whatever").offset().top;
-	var stufoff = $("section#stuff").offset().top;
+	var whatoff = $("section#dog").offset().top;
 
 	if (offset < abtoff - 50) {
 		$("#navhome").addClass("position");
@@ -59,15 +61,108 @@ $(window).scroll(function() {
 	else if (offset < whatoff - 50) {
 		$("#navabt").removeClass("position");
 		$("#navproj").addClass("position");
-		$("#navwhat").removeClass("position");
+		$("#navdog").removeClass("position");
 	}
-	else if (offset < stufoff - 50) {
+	else{
 		$("#navproj").removeClass("position");
-		$("#navwhat").addClass("position");
-		$("#navstuff").removeClass("position");
-	}
-	else {
-		$("#navwhat").removeClass("position");
-		$("#navstuff").addClass("position");
+		$("#navdog").addClass("position");
 	};
 });
+
+// get a list of everything in carousel (.find function)
+var carDiv = $(".carousel-container").find("div");
+console.log(carDiv[0]);
+var curr = 0;
+$(carDiv[1]).hide();
+$(carDiv[2]).hide();
+var leftButton = $("button#left");
+var rightButton = $("button#right");
+var past;
+leftButton.on('click', function (e) {
+	past=curr;
+	curr = ((curr - 1) + 3) % 3;
+	if (past === 0)
+	{
+		$(carDiv[curr]).fadeIn(400, function() {
+			$(carDiv[past]).fadeOut();
+		});
+	}
+	else{
+		$(carDiv[curr]).show();
+		$(carDiv[past]).fadeOut();
+	}
+	console.log("curr: "+curr)
+
+});
+rightButton.on('click', function (e) {
+	past = curr;
+	curr = (curr + 1) % 3;
+	if (past === 2)
+	{
+		$(carDiv[curr]).show();
+		$(carDiv[past]).fadeOut();
+	}
+	else {
+		$(carDiv[curr]).fadeIn(400, function() {
+			$(carDiv[past]).fadeOut();
+		});
+	}
+});
+
+var modal = $("#modal");
+var overlay = $("#overlay");
+modal.hide();
+overlay.hide();
+var bluesClue = $("#launch");
+bluesClue.on('click', function(e) {
+	var top = Math.max($(window).height() - modal.outerHeight(), 0) / 2;
+    var left = Math.max($(window).width() - modal.outerWidth(), 0) / 2;
+
+    console.log(top);
+
+    modal.css({
+        top:top + $(window).scrollTop(), 
+        left:left + $(window).scrollLeft()
+    });
+	overlay.show();
+	modal.show();
+});
+var closeButton = $("#close");
+closeButton.on('click', function(e) {
+	overlay.hide();
+	modal.hide();
+});
+
+// var modal = (function(){
+//     var method = {};
+
+//     var overlay = $('<div id="overlay"></div>');
+// 	var modal = $('<div id="modal"></div>');
+// 	var content = $('<div id="content"></div>');
+// 	var close = $('<a id="close" href="#">close</a>');
+
+// 	modal.hide();
+// 	overlay.hide();
+// 	modal.append(content, close);
+
+//     // Center the modal in the viewport
+//     method.center = function () {
+// 	    var top = Math.max($(window).height() - modal.outerHeight(), 0) / 2;
+// 	    var left = Math.max($(window).width() - modal.outerWidth(), 0) / 2;
+// 	    modal.css({
+// 	        top:top + $(window).scrollTop(), 
+// 	        left:left + $(window).scrollLeft()
+// 	    });};
+
+//     // Open the modal
+//     method.open = function (settings) {};
+
+//     // Close the modal
+//     method.close = function () {};
+
+//     return method;
+// }());
+// get transitions
+// 	posn elems absolute, set location
+// 	iterate thru list
+// 		fade one out, fade other in
